46a32896735416ad97086be3970bb28de4741206
part1zano <part1zancheg@gmail.com>
2014-01-09 12:41:45 +0300

    Added that cool #ifndef once again

--- accelgyromag.h
+++ accelgyromag.h
@@ -1,3 +1,5 @@
+#ifndef _ACCELGYROMAG_H_
+#define _ACCELGYROMAG_H_
 void initGyro(void);
 void initMag(void);
 void initAccel(void);
@@ -5,4 +7,5 @@ void initAccel(void);
 uint8_t readGyro(float *data);
 uint8_t readAccel(float *data);
 uint8_t readMag(float *data);
+#endif // _ACCELGYROMAG_H_
 
